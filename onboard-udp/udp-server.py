f0b12a30dbda29c0dc8f3981c8eb9f54b9317aba
samssi <samir.hamadi@gmail.com>
2019-06-30 01:10:21 +0300

    Make steering function

--- onboard-udp/udp-server.py
+++ onboard-udp/udp-server.py
@@ -3,10 +3,16 @@ import json
 
 host_and_port = ("0.0.0.0", 6789)
 
+
+def steer(command):
+    print(command['steering'])
+
+
 def server():
     server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     server.bind(host_and_port)
     return server
+
 
 def run(server):
     print("Starting onboard UDP server")
@@ -14,10 +20,12 @@ def run(server):
         while True:
             data, _ = server.recvfrom(1024)
             print(f"UDP message received: {data}")
-            print(json.loads(data)['steering'])
+            json_dict = json.loads(data)
+            print(json_dict)
 
     except KeyboardInterrupt:
         server.close()
         print('\nUDP server shutdown')
+
 
 run(server())
