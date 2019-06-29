b59d197d17eebabcbac75cf4461cc957250b1998
samssi <samir.hamadi@gmail.com>
2019-06-29 21:53:09 +0300

    Add ping-endpoint

--- onboard-rest/main.py
+++ onboard-rest/main.py
@@ -4,6 +4,9 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
+@app.route('/api/v1/ping', methods=['GET'])
+def ping():
+    return 'OK'
 
 @app.route('/api/v1/power', methods=['POST'])
 def power():
