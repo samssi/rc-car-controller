18710da8d1affc375ee32243a9e80953d3ee7a41
samssi <samir.hamadi@gmail.com>
2019-06-29 20:23:17 +0300

    Add resize option

--- client/main.py
+++ client/main.py
@@ -1,13 +1,14 @@
 import cv2
 
 stream = cv2.VideoCapture('http://192.168.1.127:8000/stream.mjpg')
-
+width_height = (960, 540)
 
 def start():
     while True:
         grabbed, frame = stream.read()
         if grabbed:
-            cv2.imshow("frame", frame)
+            resized = cv2.resize(frame, width_height)
+            cv2.imshow("frame", resized)
 
         k = cv2.waitKey(5) & 0xFF
         if k == 27:
