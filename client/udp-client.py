36cb5e8f2b0f0705972288baf6ca352a62354a4a
samssi <samir.hamadi@gmail.com>
2019-06-29 22:39:24 +0300

    Use remote server from local network

--- client/udp-client.py
+++ client/udp-client.py
@@ -1,8 +1,8 @@
 import socket
 import json
 
-#host_and_port = ("http://192.168.1.127", 6789)
-host_and_port = ("127.0.0.1", 6789)
+host_and_port = ("192.168.1.127", 6789)
+#host_and_port = ("127.0.0.1", 6789)
 client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 def json_control_command():
