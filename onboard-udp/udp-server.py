55544de6eebd1b5eaffdfaf7576350f47d182e01
samssi <samir.hamadi@gmail.com>
2019-07-18 23:18:49 +0300

    Add cutoff thread to complement UDP connection loss from the client

--- onboard-udp/udp-server.py
+++ onboard-udp/udp-server.py
@@ -1,8 +1,20 @@
 import socket
 import json
-import RPi.GPIO as GPIO
+import time
+import threading
+#import RPi.GPIO as GPIO
+from config import Settings
+from state import CarState
 
-host_and_port = ("0.0.0.0", 6789)
+carState = CarState()
+
+settings = Settings('settings.ini')
+host = settings.getParser().get('default', 'udp_server_accept_host')
+port = settings.getParser().getint('default', 'udp_server_port')
+
+cutoff_polling_frequency_in_secs = settings.getParser().getint('default', 'cutoff_polling_frequency_in_secs')
+
+host_and_port = (host, port)
 
 # See: http://www.ti.com/lit/ds/symlink/l293.pdf
 enable_channel_1_and_2 = 3
@@ -13,23 +25,27 @@ enable_channel_3_and_4 = 5
 driver_input_3A = 11
 driver_input_4A = 7
 
-GPIO.setmode(GPIO.BOARD)
-GPIO.setwarnings(False)
-
-GPIO.setup(enable_channel_1_and_2, GPIO.OUT)
-GPIO.setup(driver_input_1A, GPIO.OUT)
-GPIO.setup(driver_input_2A, GPIO.OUT)
-
-GPIO.setup(enable_channel_3_and_4, GPIO.OUT)
-GPIO.setup(driver_input_3A, GPIO.OUT)
-GPIO.setup(driver_input_4A, GPIO.OUT)
-
-GPIO.output(enable_channel_1_and_2, GPIO.HIGH)
-GPIO.output(enable_channel_3_and_4, GPIO.HIGH)
-
-driver_input_1A_pwm = GPIO.PWM(driver_input_1A, 7500)
+#GPIO.setmode(GPIO.BOARD)
+#GPIO.setwarnings(False)
+#
+#GPIO.setup(enable_channel_1_and_2, GPIO.OUT)
+#GPIO.setup(driver_input_1A, GPIO.OUT)
+#GPIO.setup(driver_input_2A, GPIO.OUT)
+#
+#GPIO.setup(enable_channel_3_and_4, GPIO.OUT)
+#GPIO.setup(driver_input_3A, GPIO.OUT)
+#GPIO.setup(driver_input_4A, GPIO.OUT)
+#
+#GPIO.output(enable_channel_1_and_2, GPIO.HIGH)
+#GPIO.output(enable_channel_3_and_4, GPIO.HIGH)
 
 # DC electric motors: 5-10 kHz or higher
+#driver_input_1A_pwm = GPIO.PWM(driver_input_1A, 7500)
+
+
+def cut_engine():
+    print('Engine cut!')
+
 def steer(command):
     if command['steering']['direction'] == 'left':
         GPIO.output(driver_input_3A, GPIO.HIGH)
@@ -51,8 +67,23 @@ def server():
     return server
 
 
+def cutoff():
+    thread = threading.currentThread()
+
+    while getattr(thread, "do_run", True):
+        if carState.cutoff_threshold_passed():
+            cut_engine()
+
+        time.sleep(cutoff_polling_frequency_in_secs)
+
+    print('Cutoff thread finished')
+
+
 def run(server):
     print("Starting onboard UDP server")
+    cutoff_thread = threading.Thread(target=cutoff)
+    cutoff_thread.start()
+
     try:
         while True:
             data, _ = server.recvfrom(1024)
@@ -62,7 +93,10 @@ def run(server):
 
     except KeyboardInterrupt:
         server.close()
+        cutoff_thread.do_run = False
+        cutoff_thread.join()
         print('\nUDP server shutdown')
 
 
-run(server())
+if __name__ == "__main__":
+    run(server())
